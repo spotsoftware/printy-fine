df811775b5909a112dce88588465c37bb0b927c6
Marco Ceccarelli <marco.ceccarelli@spot.it>
2014-12-06 19:48:24 +0100

    Update Service Controller

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ var connectAssets = require('connect-assets');
 
 var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
+var serviceController = require('./controllers/service');
 var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
 
@@ -148,6 +149,11 @@ app.post('/account/profile', passportConf.isAuthenticated, userController.postUp
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
+
+app.get('/account/services', passportConf.isAuthenticated, serviceController.getUserServices);
+app.post('/account/service/:serviceId', passportConf.isAuthenticated, serviceController.postUserService);
+
+
 
 /**
  * API examples routes.
