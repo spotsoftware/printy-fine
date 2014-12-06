9201c0a641e0d6b4f634fd85d18fe7de32baf67e
Alessandro Molari <molari.alessandro@gmail.com>
2014-12-06 15:23:51 +0100

    Footer v2

--- helpers/links.js
+++ helpers/links.js
@@ -1,16 +1,21 @@
 exports.filtered = function(user) {
-  var filteredLinks = {};
+  var filteredLinks = [];
 
   if (!user) {
-    filteredLinks['/login'] = { title: 'Login', icon: 'sign-in' };
-    filteredLinks['/signup'] = { title: 'Create Account', icon: 'user' };
+    filteredLinks.push({ url: '/login', title: 'Login', icon: 'sign-in' });
+    filteredLinks.push({ url: '/signup', title: 'Create Account', icon: 'user' });
   } else {
     if (user.kind === 'user') {
-      filteredLinks['/aggregator'] = { title: 'Aggregator', icon: 'archive' };
+      filteredLinks.push({ url: '/aggregator', title: 'Aggregator', icon: 'archive' });
     }
     if (user.kind === 'service') {
-      filteredLinks['/fineprints/new'] = { title: 'Create FinePrint', icon: 'file' };
+      filteredLinks.push({ url: '/fineprints/new', title: 'Create FinePrint', icon: 'file' });
     }
+    if (filteredLinks.length > 0) {
+      filteredLinks.push('separator');
+    }
+    filteredLinks.push({ url: '/account', title: 'My Account', icon: 'user' });
+    filteredLinks.push({ url: '/logout', title: 'Logout', icon: 'sign-out' });
   }
 
   return filteredLinks;
