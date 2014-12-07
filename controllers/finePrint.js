98cc95e88490e6fe98a696f66195dc7dce1f3dbe
Marco Ceccarelli <marco.ceccarelli@spot.it>
2014-12-07 22:52:17 +0100

    Scroll Page and Tags Udpate

--- controllers/finePrint.js
+++ controllers/finePrint.js
@@ -11,37 +11,45 @@ var _tags = [
     {
         code: 'no-privacy',
         title: 'There is no Privacy',
-        icon: 'http://loremicon.com/i/flat/64/security'
+        icon: 'http://loremicon.com/i/flat/64/security',
+        description: 'With the subscription of this service, you agree to NO MORE Privacy.'
+        
     },
     {
         code: 'time',
         title: 'Your time is our',
-        icon: 'http://loremicon.com/i/flat/64/clock'
+        icon: 'http://loremicon.com/i/flat/64/clock',
+        description: 'Time is an overvalued concept. Starting from now you agree to give us your time. Our day will be 25h.'
     },
     {
         code: 'photo-property',
         title: 'All images is property of the service',
-        icon: 'http://loremicon.com/i/flat/64/image '
+        icon: 'http://loremicon.com/i/flat/64/image ',
+        description: 'All images uploaded to this service, will become property of the service itself. You will not have no property.'
     },
     {
         code: 'automatic-updates',
         title: 'There is no Privacy',
-        icon: 'http://loremicon.com/i/flat/64/download '
+        icon: 'http://loremicon.com/i/flat/64/download ',
+        description: 'We will update service without any comunication. We can left features as we need.'
     },
     {
         code: 'sold-souls',
         title: 'Your soul is property of this service',
-        icon: 'http://loremicon.com/i/flat/64/money'
+        icon: 'http://loremicon.com/i/flat/64/money',
+        description: 'You will sold us your soul, completely without any choice of compensation.'
     },
     {
         code: 'credit-card',
         title: 'We can use your credit card as we need',
-        icon: 'http://loremicon.com/i/flat/64/creditcard'
+        icon: 'http://loremicon.com/i/flat/64/creditcard',
+        description: 'You must share with us your credit card pin code, we will use it many times.'
     },
     {
         code: 'home-key',
         title: 'We have a copy of your home\'s keys',
-        icon: 'http://loremicon.com/i/flat/64/key'
+        icon: 'http://loremicon.com/i/flat/64/key',
+        description: 'You must make a copy of your home\'s keys. We will evaulate it as our accomodation.'
     },
      
 ];
